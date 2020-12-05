5583af77cad61f5582de60277dab9d5a6245d99c
HugoUnosson <75093627+HugoUnosson@users.noreply.github.com>
2020-12-05 15:17:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+               
+            foreach(char i in input)
+            {
+                if (i != 'h')
+                {
+                    Console.Write(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            foreach (char i in input.Reverse())
+            {
+                if (i != 'h')
+                {
+                    Console.Write(i);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
